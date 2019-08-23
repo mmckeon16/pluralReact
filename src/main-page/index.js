a3cf2c5afe5b2c5d854e36e8c72fea02988b4c91
Mary Mckeon <mmckeo@a1990-14280.icims.net>
2019-08-23 17:21:55 -0400

    search bar crashes when getting results

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './main-page.css';
 import Header from './header';
 import FeaturedHouse from './featured-house';
+import HouseFilter from './house-filter';
+import SearchResults from '../search-results';
+import HouseDetail from '../house';
 
 class App extends Component {
   state = {};
@@ -17,6 +20,7 @@ class App extends Component {
     .then(allHouses => {
       this.allHouses = allHouses;
       this.determineFeaturedHouse();
+      this.determineUniqueCountries();
     })
   }
 
@@ -28,11 +32,40 @@ class App extends Component {
     };
   }
 
+  determineUniqueCountries = () => {
+    const countries = this.allHouses
+      ? Array.from(new Set(this.allHouses.map(h => h.country)))
+      : [];
+    countries.unshift(null);
+    this.setState({countries});
+  }
+
+  filterHouses = (country) => {
+    this.setState({activeHouse: null});
+    const filteredHouses = this.allHouses.filter((h) => h.country ===country);
+    this.setState({filteredHouses});
+    this.setState({country});
+  }
+
+  setActiveHouse = (house) => {
+    this.setState({activeHouse :house});
+  }
+
   render() {
+    let activeComponent = null;
+    if(this.state.country) {
+      activeComponent = <SearchResults country = {this.state.country}
+        filteredHouses={this.state.filterHouses} setActiveHouse = {this.setActiveHouse}/>
+    }
+    if(this.state.activeHouse)
+      activeComponent = <HouseDetail house = {this.state.activeHouse}/>
+    if(!activeComponent)
+      activeComponent = <FeaturedHouse house = {this.state.featuredHouse} />
     return (
       <div className="container">
         <Header subtitle = "Providing Houses world wide baby!"/>
-        <FeaturedHouse house={this.state.featuredHouse} />
+        <HouseFilter countries={this.state.countries} filterHouses={this.filterHouses} />
+        {activeComponent}
       </div>
     );
     }
